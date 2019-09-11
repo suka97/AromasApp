a903ebeb6fd2fbea5183703abfeabc88ce3fe19f
suka97 <asukanec@est.frba.utn.edu.ar>
2019-09-11 01:14:19 -0300

    lesto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { IonicStorageModule } from "@ionic/storage";
 
 import { HomePage } from './home.page';
 
@@ -10,6 +11,7 @@ import { HomePage } from './home.page';
   imports: [
     CommonModule,
     FormsModule,
+    IonicStorageModule.forRoot(),
     IonicModule,
     RouterModule.forChild([
       {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
-import { IonCard, IonButton } from "@ionic/angular";
+import { IonCard, IonButton, ToastController  } from "@ionic/angular";
+import { Storage } from "@ionic/storage";
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-home',
@@ -8,31 +10,70 @@ import { IonCard, IonButton } from "@ionic/angular";
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private storage: Storage, public toastController: ToastController) {}
 
   @ViewChildren(IonCard) cards !: QueryList<IonCard>
-  @ViewChild('card_0', {static: false}) card !: IonCard;
+  @ViewChild('card_0', {static: false}) card !: IonCard
   @ViewChild('connectBtn', {static: false}) connectBtn !: IonButton
 
-  url: string = "localhost";  //"192.168.1.128";
+  url: string = "192.168.1.108";  //"localhost";  //"192.168.1.128";
+  port: string = "3001";
   connection: WebSocket;
-  time: string[] = ["00:00:01", "00:00:02", "00:00:03", "00:00:04"];
-  state: string[] = ["LED_OFF", "LED_OFF", "LED_OFF", "LED_OFF"];
+  time: string[] = ["500", "1000", "1500", "2000"];
+  nombreAroma: string[] = ["", "", "", ""];
 
   ngAfterViewInit() {
-    this.connect();
+    this.getDefaults().then(_=> {
+      this.connect();
+    });
   }
 
-  connect() {
+  getDefaults() {
+    return Promise.all([
+      this.storage.get("url"), this.storage.get("times"), 
+      this.storage.get("port"), this.storage.get("nombreAromas")
+    ]).then(values => {
+      console.log(values);
+        if ( values[0] != null )
+          this.url = values[0];
+
+        if ( values[1] != null ) {
+          for( let i=0 ; i<this.time.length ; i++ ) {
+            if ( values[1][i] != null ) 
+              this.time[i] = values[1][i]
+          }
+        }
+
+        if ( values[2] != null )
+          this.port = values[2];
+
+        if ( values[3] != null ) {
+          for( let i=0 ; i<this.time.length ; i++ ) {
+            if ( values[3][i] != null ) 
+              this.nombreAroma[i] = values[3][i]
+          }
+        }
+    });
+  }
+
+  connect() { 
+    console.log("URL = "+ this.url + "\nPORT = " + this.port);
+
+    /*  Guardo los datos  */
+    this.storage.set('url', this.url);
+    this.storage.set('port', this.port);
+    this.storage.set('nombreAromas', this.nombreAroma);
+    this.storage.set('times', this.time);
+
     if ( this.connection != null ) {
-      if ( this.connection.OPEN ) {
-        this.connection.close();
-      }
+      console.log("Connection closed");
+      this.connection.close();
     }
     
-    this.connection = new WebSocket("ws://" + this.url + ":3001/");
+    this.connection = new WebSocket("ws://" + this.url + ":" + this.port + "/");
     this.connection.onopen = (e)=>{
       console.log("Connected");
+      this.toast("Connected");
       this.connectBtn.color = "primary";
 
       this.connection.onclose = (e)=>{
@@ -40,25 +81,34 @@ export class HomePage {
       };
       this.connection.onmessage = (e)=>{
         console.log("Server: " + e.data);
+        let command: string[] = e.data.split("&");
 
-        if ( e.data == "LED_OFF" ) { 
-          this.state[0] = e.data;
-          this.card.color = "light";
+        if ( command[1] == "OFF" ) { 
+          this.cards[+command[0]] = "light";  // el '+' lo pasa a int
         }
-        else if ( e.data == "LED_ON" ) {
-          this.state[0] = e.data;
-          this.card.color = "danger";
+        else if ( command[1] == "ON" ) {
+          this.cards[+command[0]] = "danger";  // el '+' lo pasa a int
         }
       };
+    };
+    this.connection.onclose= (e) =>{
+      console.log("Disconnected");
+      this.toast("Disconnected");
     };
   }
 
   clicked(index: number) {
-    let millis: string = this.time[index].split(":")[2];
-    console.log("time: " + millis);
-    let nextState: string = (this.state[index] == "LED_OFF") ? "LED_ON" : "LED_OFF";
-    this.connection.send(nextState);
-    this.state[index] = nextState;
+    let millis: string = this.time[index];
+    //console.log("time: " + millis);
+    this.connection.send(index + "&" + millis);
+  }
+
+  async toast(message: string, duration: number = 2000) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration
+    });
+    toast.present();
   }
 
 }
