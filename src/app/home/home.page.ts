4399f22c55dbe85777b15de000f01b873a221dfb
suka97 <andressukanec@hotmail.com>
2019-09-13 19:54:43 -0300

    al fin anda todo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
-import { IonCard, IonButton, ToastController  } from "@ionic/angular";
+import { IonCard, IonButton, ToastController, LoadingController   } from "@ionic/angular";
 import { Storage } from "@ionic/storage";
+import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,22 +10,30 @@ import { Storage } from "@ionic/storage";
 })
 export class HomePage {
 
-  constructor(private storage: Storage, public toastController: ToastController) {}
+  constructor(private storage: Storage, public toastController: ToastController, public loadingController: LoadingController) {}
 
   @ViewChildren(IonCard) cards !: QueryList<IonCard>
   @ViewChild('card_0', {static: false}) card !: IonCard
   @ViewChild('connectBtn', {static: false}) connectBtn !: IonButton
 
-  url: string = "192.168.1.108";  //"localhost";  //"192.168.1.128";
-  port: string = "3001";
+  url: string = "192.168.1.1";  //"localhost";  //"192.168.1.128";
+  port: string = "80";
   connection: WebSocket;
   time: string[] = ["500", "1000", "1500", "2000"];
   nombreAroma: string[] = ["", "", "", ""];
+  connected: boolean = false;
+  checkConnectionLoop: any;
+  isLoading: boolean = false;
 
   ngAfterViewInit() {
-    this.getDefaults().then(_=> {
-      this.connect();
-    });
+    this.getDefaults().then(_=> {  });
+    // check connection loop
+    this.checkConnectionLoop = setInterval(() => {
+      if ( this.connected ) {
+        this.connection.send("check");
+        console.log("check");
+      }
+    }, 1000);
   }
 
   getDefaults() {
@@ -58,48 +67,56 @@ export class HomePage {
   connect() { 
     console.log("URL = "+ this.url + "\nPORT = " + this.port);
 
+    if ( this.connected ) {
+      if ( this.connection != null ) {
+        console.log("Connection closed");
+        this.connection.close();
+      }
+      this.connected = false;
+      return;
+    }
+
     /*  Guardo los datos  */
     this.storage.set('url', this.url);
     this.storage.set('port', this.port);
     this.storage.set('nombreAromas', this.nombreAroma);
     this.storage.set('times', this.time);
-
-    if ( this.connection != null ) {
-      console.log("Connection closed");
-      this.connection.close();
-    }
     
     this.connection = new WebSocket("ws://" + this.url + ":" + this.port + "/");
+    this.presentLoading("Conectando");
     this.connection.onopen = (e)=>{
+      this.connected = true;
       console.log("Connected");
       this.toast("Connected");
       this.connectBtn.color = "primary";
-
-      this.connection.onclose = (e)=>{
-        this.connectBtn.color = "danger";
-      };
-      this.connection.onmessage = (e)=>{
-        console.log("Server: " + e.data);
-        let command: string[] = e.data.split("&");
-
-        if ( command[1] == "OFF" ) { 
-          this.cards[+command[0]] = "light";  // el '+' lo pasa a int
-        }
-        else if ( command[1] == "ON" ) {
-          this.cards[+command[0]] = "danger";  // el '+' lo pasa a int
-        }
-      };
+      this.dismissLoading();
     };
-    this.connection.onclose= (e) =>{
+    this.connection.onclose = (e)=>{
+      this.dismissLoading();
+      this.connected = false;
+      this.connectBtn.color = "danger";
       console.log("Disconnected");
       this.toast("Disconnected");
+    };
+    this.connection.onmessage = (e)=>{
+      console.log("Server: " + e.data);
+      let command: string[] = e.data.split("&");
+
+      if ( command[1] == "OFF" ) { 
+        this.cards[+command[0]] = "light";  // el '+' lo pasa a int
+      }
+      else if ( command[1] == "ON" ) {
+        this.cards[+command[0]] = "danger";  // el '+' lo pasa a int
+      }        
     };
   }
 
   clicked(index: number) {
-    let millis: string = this.time[index];
-    //console.log("time: " + millis);
-    this.connection.send(index + "&" + millis);
+    if ( this.connected ) {
+      let millis: string = this.time[index];
+      //console.log("time: " + millis);
+      this.connection.send(index + "&" + millis);
+    }
   }
 
   async toast(message: string, duration: number = 2000) {
@@ -108,6 +125,25 @@ export class HomePage {
       duration: duration
     });
     toast.present();
+  }
+
+  async presentLoading(message: string, duration: number = 30000) {
+    this.isLoading = true;
+    return await this.loadingController.create({
+      message: message,
+      duration: duration
+    }).then(a => {
+      a.present().then(() => {
+        //console.log('presented');
+        if (!this.isLoading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
+    });
+  }
+  async dismissLoading() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
   }
 
 }
